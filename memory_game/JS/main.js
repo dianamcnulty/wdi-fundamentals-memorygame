08e374c5c41efc02a4d36ed6aefccc774c851bdb
Diana <diana.mcnulty@libertymutual.com>
2017-08-15 12:59:16 -0400

    created game logic

---
+++ memory_game/JS/main.js
@@ -0,0 +1,8 @@
+console.log("Up and running!");
+var cardOne = "king";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "queen";
+
+console.log("User flipped " + cardOne)
+console.log("User flipped " + cardThree)
