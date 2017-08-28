75beb705a3645228a7b2ff867fc369ff327419a4
Diana <diana.mcnulty@libertymutual.com>
2017-08-28 13:56:19 -0400

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,8 +1,19 @@
 console.log("Up and running!");
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
 
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[3];
+var cardTwo = cards[2]
+
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne)
-console.log("User flipped " + cardThree)
+cardsInPlay.push(cardTwo)
+console.log("User flipped " + cardTwo)
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0]=== cardsInPlay[1]){
+		alert("You found a match!")
+	}else{
+		alert("Sorry, try again.")
+	}
+}
