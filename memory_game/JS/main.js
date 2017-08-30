4483c708206071327c229f2a8a88922f2af91667
Diana <diana.mcnulty@libertymutual.com>
2017-08-30 14:46:33 -0400

    added card objects

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,6 +1,27 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds"
+}
+];
 var cardsInPlay = [];
 var checkForMatch = function(){
 	if (cardsInPlay[0]=== cardsInPlay[1]){
@@ -10,8 +31,10 @@ var checkForMatch = function(){
 		}
 }
 var flipCard = function (cardId){
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
 	if (cardsInPlay.length === 2){
 		checkForMatch();
